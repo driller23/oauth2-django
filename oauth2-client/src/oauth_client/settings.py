12ccfdce5317e8ec2e8f00112bbddb16171b5305
Igal Dahan <igal.dahan@gmail.com>
2024-09-11 21:04:59 +0300

    Update settings.py

--- oauth2-client/src/oauth_client/settings.py
+++ oauth2-client/src/oauth_client/settings.py
@@ -7,6 +7,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or get_random_secret_key()
 
 DEBUG = os.environ.get('DEBUG', '0') == '1'
 
+# Add this to the existing settings.py file
+STATIC_URL = '/static/'
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
