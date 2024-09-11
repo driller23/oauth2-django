b3b6d459c727aa4c77a3bd5bea96ebb6fe7db645
Igal Dahan <igal.dahan@gmail.com>
2024-09-11 21:23:52 +0300

    Update settings.py

--- oauth2-server/src/oauth_server/settings.py
+++ oauth2-server/src/oauth_server/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or get_random_secret_key()
 
 DEBUG = os.environ.get('DEBUG', '0') == '1'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'oauth2-server']
 
 # Add this to the existing settings.py file
 STATIC_URL = '/static/'
