8d6362c80eb82ec13a66d8f62c0625f1a9f61162
Igal Dahan <igal.dahan@gmail.com>
2024-09-11 21:27:47 +0300

    Update views.py

--- oauth2-server/src/oauth_server/views.py
+++ oauth2-server/src/oauth_server/views.py
@@ -1,6 +1,24 @@
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 from oauth2_provider.decorators import protected_resource
 from django.http import JsonResponse
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    authentication_form = AuthenticationForm
 
 @protected_resource(scopes=['read'])
 def api_hello(request):
     return JsonResponse({"message": "Hello, OAuth2 World!"})
+
+# oauth2-server/src/oauth_server/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('api/hello/', views.api_hello, name='api_hello'),
+    path('accounts/login/', views.CustomLoginView.as_view(), name='login'),
+]
